be846fbebe804e3e35d9134be62aaf4ec15fd214
Jacob Zuo <chopinsky@live.com>
2017-09-23 18:44:49 -0500

    adding message module

---
+++ renderer/components/Message.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "./Warnings.css";
+
+class Message extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="msgWrapper">
+        
+      </div>
+    );
+  }
+}
