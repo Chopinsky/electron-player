e8cf5188353dcea2ed2a9503b27df53eddb5a46e
Jacob Zuo <chopinsky@live.com>
2017-09-23 21:19:07 -0500

    adding more contents to the message module

--- renderer/components/Message.js
+++ renderer/components/Message.js
@@ -4,12 +4,28 @@ import "./Warnings.css";
 class Message extends Component {
   constructor(props) {
     super(props);
+
+    this.state = { "message": null };
+    
+    this.onBtnClicked = this.onBtnClicked.bind(this);
+  }
+
+  componentWillReceiveProps(nextProps) {
+  }
+
+  onBtnClicked(event) {
+    event.stopPropagation();
+    event.preventDefault();
+
+    this.setState({ "message": null });
   }
 
   render() {
     return (
-      <div className="msgWrapper">
-        
+      <div className="msgWrapper msgTransition msgError" 
+           style={{ "display": this.state.message ? "inline-block" : "none" }}>
+        <span>{this.state.message}</span>
+        <button className="msgBtn msgBtnTransition" onSubmit={this.onBtnClicked}>OK</button>
       </div>
     );
   }
